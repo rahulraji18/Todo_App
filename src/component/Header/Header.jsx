51075b4e251878ae4510d9e4301dab115cd59470
rahulraji18 <rahulrajesh474@gmail.com>
2023-11-15 09:22:34 +0530

    path changes

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <Link to="/">Home</Link>
+      <Link to="/Todo_App">Home</Link>
       <Link to="/about">About</Link>
     </header>
   );
